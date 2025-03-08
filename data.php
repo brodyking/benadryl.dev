0dbe423e6f85db1f144c09c89e672735a4d8eb79
brody king <46272894+brodyking@users.noreply.github.com>
2025-03-08 15:42:43 -0600

    Upload

---
+++ data.php
@@ -0,0 +1,14 @@
+<?php
+$datajson = file_get_contents("data.json");
+$data = json_decode($datajson,true);
+
+function siteName()
+{
+    global $data;
+    return $data["siteName"];
+}
+function siteMotd() {
+    global $data;
+    return $data["siteMotd"];
+}
+?>
