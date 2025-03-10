22d85cdc63ce3a3bbf59e1b13c634844e49148ae
brody king <46272894+brodyking@users.noreply.github.com>
2025-03-09 20:16:05 -0500

    rename to config

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+$datajson = file_get_contents("config.json");
+$data = json_decode($datajson,true);
+
+function siteName()
+{
+    global $data;
+    return $data["siteName"];
+}
+function siteMotd() {
+    global $data;
+    return $data["siteMotd"];
+}
+function footerText() {
+    global $data;
+    return $data["footerText"];
+}
+?>
